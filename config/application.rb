05e13252f5a227589a5e2f1f05eee24be1432c1e
Jane-Terziev <janeterziev@gmail.com>
2024-11-09 23:52:25 +0700

    Fix zeitwerk error

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ module AppName
   class Application < Rails::Application
     config.hosts << /.*/
     config.load_defaults 8.0
+    Rails.autoloaders.main.ignore(Rails.root.join("lib/generators/engines/engine_generator.rb"))
     config.autoload_lib(ignore: %w[assets tasks])
     config.eager_load_paths << "#{config.root}/lib/utils"
   end
